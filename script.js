e99b9c40325908e6e6a0a2b6e4c7d64fc01a7f24
Julia Evans <julia@jvns.ca>
2023-07-15 15:04:36 -0400

    refresh data every 2 minutes

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ const map = new mapboxgl.Map({
 });
 
 map.on("load", setup);
-// start loading inventory asap
 
 function setup() {
   geolocate();
-  addStations();
+  update();
 }
 
 function geolocate() {
@@ -56,7 +55,9 @@ function createMarker(feature) {
   new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).setPopup(
     new mapboxgl.Popup({ offset: 25 }) // add popups
       .setHTML(
-        `<h3>${station.name}</h3><p>
+        `
+        <div class="time">${formatTime(station.last_reported)}</div>
+        <h3>${station.name}</h3><p>
             <div class="info">
                <div class="info__item">
                   <div class="num"> ${station.bikes_available} </div>
@@ -77,10 +78,45 @@ function createMarker(feature) {
   ).addTo(map);
 }
 
-async function addStations() {
-  const response = await inventory;
-  const geojson = await response.json();
+function formatTime(utc_timestamp) {
+  /* format in a nice way, like x seconds/minutes ago */
+  const now = new Date();
+  const timestamp = new Date(utc_timestamp * 1000);
+  const diff = now - timestamp;
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  if (minutes == 1) {
+    return "1 minute ago";
+  } else if (minutes < 1) {
+    return `${seconds} seconds ago`;
+  }
+  return `${minutes} minutes ago`;
+}
+
+function addStations(geojson) {
+  /* delete all markers */
+  document.querySelectorAll(".marker").forEach((marker) => marker.remove());
   for (const feature of geojson.features) {
     createMarker(feature);
+  }
+}
+
+function sleep(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+async function update() {
+  while (true) {
+    console.log("refreshing...");
+    const response = await fetch(
+      "https://layer.bicyclesharing.net/map/v1/mtl/map-inventory",
+    );
+    const geojson = await response.json();
+    addStations(geojson);
+    /* every 10 seconds for 2 minutes, refresh the data */
+    for (let i = 0; i < 12; i++) {
+      await sleep(10);
+      addStations(geojson);
+    }
   }
 }
